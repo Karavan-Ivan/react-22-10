12f8d9ba0b2d5fa034b94d0c5b585b08146cdcd3
Vania <ikaravanv@gmail.com>
2023-03-01 18:54:45 +0200

    inside style

--- src/List.tsx
+++ src/List.tsx
@@ -1,5 +1,6 @@
 type Props = {}
 const List = (props: Props) => {
+    let data = true
     return (
         <>
             <ul>
@@ -7,11 +8,23 @@ const List = (props: Props) => {
                 <li>list</li>
                 <li>list</li>
             </ul>
-            <p>
+            <p
+                style={{
+                    color: 'white',
+                    backgroundColor: data ? 'purple' : 'tomato',
+                    padding: '15px',
+                }}
+            >
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit,
                 dolor nulla similique veniam voluptatum ratione maiores totam
                 sint accusantium id cumque nemo earum quisquam itaque? Cumque
                 ipsa aperiam consequatur quis!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Architecto accusamus, consectetur animi pariatur temporibus
+                illum quaerat voluptatum sequi error, aspernatur beatae id magni
+                amet ea consequatur, adipisci unde exercitationem laboriosam?
             </p>
         </>
     )
